17383d7885f52c8a9e4450d393334a24fa050feb
Raj <anuragkumar774962@gmail.com>
2023-08-16 03:06:31 +0530

    removed LOTTie files

--- Frontend/src/component/Loading.js
+++ Frontend/src/component/Loading.js
@@ -1,20 +1,13 @@
 import React from 'react'
-import Lottie from 'react-lottie';   
-import * as animationData from './loader.json'
+
+
+import loader from "../component/infinity.gif"
 export default function Loading() {   
-    const defaultOptions = {
-        loop: true,
-        autoplay: true, 
-        animationData: animationData,
-        rendererSettings: {
-          preserveAspectRatio: 'xMidYMid slice'
-        }
-      };
+ 
   return (
-    <div> 
-<Lottie options={defaultOptions}
-              height={400}
-              width={400}/>
+    <div>   
+      <img src={loader} alt="loading" className='Loader' />
+\
 
     </div>
   )
